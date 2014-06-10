f802963985deaa5d301cf88c0d0f15b1088d21a0
braun <braun@brauns-iMac.local>
2014-06-09 20:02:54 -0400

    added protractor and app suite

---
+++ test/e2e/appSuite.js
@@ -0,0 +1,15 @@
+describe('Workout Metrics',function(){
+
+	it('should have more than zero workouts',function(){
+		browser
+			.get('http://workout-metrics.com:3000/#/workouts/read')
+		;
+		element
+			.all(by.repeater('w in workouts'))
+			.then(function(workouts){
+				expect(workouts.count()).toBe(true);
+			})
+		;
+	});
+
+});
