b61ff964d65aca4e4e991f7bfacd36efc417cdc8
braun <braun@brauns-iMac.local>
2014-06-10 20:38:16 -0400

    bug

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function WorkoutsMetricsController ($scope,$http,WorkoutsService) {
 		var w = $scope.workouts;
 		var l = $scope.data.labels;
 		for (var i in w) {
-			l.push(w[i]);
+			l.push(w[i].wstart);
 		}
 		var ctx = document.getElementById("workout-metrics-spline").getContext("2d");
 		var myNewChart = new Chart(ctx).Line($scope.data);
