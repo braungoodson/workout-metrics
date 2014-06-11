7c4e21356611267887ff1858bb703b92a259fad3
braun <braun@brauns-iMac.local>
2014-06-10 20:48:12 -0400

    bug

--- index.js
+++ index.js
@@ -47,9 +47,14 @@ function WorkoutsMetricsController ($scope,$http,WorkoutsService) {
 		.getWorkouts()
 		.then(onResolve,onReject,onNotify)
 	;
-	$scope.$watch('data',function(newValue,oldValue){
+	$scope.$watch('workouts',function(){
+		$scope.data.labels = [];
+		var l = $scope.data.labels;
+		for (var i in w) {
+			l.push(w[i].wstart);
+		}
 		var ctx = document.getElementById("workout-metrics-spline").getContext("2d");
-		var myNewChart = new Chart(ctx).Line(newValue);
+		var myNewChart = new Chart(ctx).Line($scope.data);
 	});
 	function onNotify (notification) {
 		console.log('n',notification);
@@ -64,6 +69,8 @@ function WorkoutsMetricsController ($scope,$http,WorkoutsService) {
 		for (var i in w) {
 			l.push(w[i].wstart);
 		}
+		var ctx = document.getElementById("workout-metrics-spline").getContext("2d");
+		var myNewChart = new Chart(ctx).Line($scope.data);
 	}
 }
 
